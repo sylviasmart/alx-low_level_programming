a851d2cb6e9d64b1e52a7ca620a3155f21f17afd
Slyviasmart <slyviasmartalozie@gmail.com>
2023-06-15 17:52:09 -0700

    printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,2 @@
 #!/bin/bash
-int main (void) {print("with proper grammar, but the outcome is a piece of art,\n"); return (0); }}
+int main (void) {printf("with proper grammar, but the outcome is a piece of art,\n"); return (0); }}
