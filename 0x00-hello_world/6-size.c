208f4ce284852e6f619a6223afccc6b6634c4e6a
Slyviasmart <slyviasmartalozie@gmail.com>
2023-06-15 18:41:31 -0700

    size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,7 @@
 /**
  *  main - printing out the size of computer types
  *   Return: 0
- *   */
+ */
 int main(void)
 {
 	char a;
@@ -10,6 +10,7 @@ int main(void)
 	long int c;
 	long long int d;
 	float f;
+
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
