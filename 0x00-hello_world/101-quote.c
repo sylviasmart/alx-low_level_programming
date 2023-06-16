c10131b5c63bd21ee0d7b48a886f09b1019d2baf
Slyviasmart <slyviasmartalozie@gmail.com>
2023-06-15 19:06:12 -0700

    quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
- * main -maincode
- * Return: 1
- */
+ *  main - printing a line to the standard error
+ *   Return: 1
+ *   */
 int main(void)
 {
-	fwrite(2,"and that piece of art is useful" - Dora Korpar, 2015-10-19\n",61);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
 }
