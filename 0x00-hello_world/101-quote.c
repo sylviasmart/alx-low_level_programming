2d74342d5bb9c9de371abf22c94c11008f81d1ac
Slyviasmart <slyviasmartalozie@gmail.com>
2023-06-15 18:59:04 -0700

    quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,5 +5,5 @@
  */
 int main(void)
 {
-	write(2,"and that piece of art is useful" - Dora Korpar, 2015-10-19\n",61);
+	fwrite(2,"and that piece of art is useful" - Dora Korpar, 2015-10-19\n",61);
 }
