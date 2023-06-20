d5c631c18957989674e1975dd0b317570154bc77
Slyviasmart <Slyviasmartalozie@gmail.com>
2023-06-19 17:23:25 -0700

    3print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - print alphabets in lower and upper case
+ * Return: 0
+ */
+int main(void)
+{
+	char all;
+	char AL;
+
+	all = 'a';
+	AL = 'A';
+	while (all <= 'z')
+	{
+		putchar(all);
+		all++;
+	}
+	while (AL <= 'Z')
+	{
+		putchar(AL);
+		AL++
+	}
+	putchar('\n');
+	return (0);
+}
