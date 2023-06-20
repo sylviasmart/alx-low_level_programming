ff9609dfff0433cfc6a93122224cc9299a25b777
Slyviasmart <Slyviasmartalozie@gmail.com>
2023-06-19 18:29:57 -0700

    100printcomb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - printing double number combinations
+ * Return: 0
+ */
+int main(void)
+{
+	int f, s;
+
+	for (f = 0; f < 10; f++)
+	{
+		for (s = 0; s < 10; s++)
+		{
+			if (f * 10 + s < s * 10 + f)
+			{
+				putchar(f + '0');
+				putchar(s + '0');
+				if (f * 10 + s != 89)
+				{																																		putchar(',');
+					putchar(' ');
+				}
+			}
+			putchar('\n');
+			return (0);
+
+
+
+
