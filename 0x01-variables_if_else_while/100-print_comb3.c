e86a6d0c7a5e2dbaab702627381723df14aa9d99
Slyviasmart <Slyviasmartalozie@gmail.com>
2023-06-19 18:36:01 -0700

    100printcomb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -19,10 +19,9 @@ int main(void)
 				{																																		putchar(',');
 					putchar(' ');
 				}
-			}
-			putchar('\n');
-			return (0);
-
-
-
-
+				}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
