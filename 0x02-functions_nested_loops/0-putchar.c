06893dbb708060085dd25c984be7f5eaca287a41
Slyviasmart <Slyviasmartalozie@gmail.com>
2023-06-21 04:55:10 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,8 +3,15 @@
  * main -print char follwed by new line
  * Return: 0
  */
-int main(void)
-{
-	putchar('\n');
+int main(void) 
+	_putchar(_); 
+	_putchar(p); 
+	_putchar(u); 
+	_putchar(t); 
+	_putchar(c); 
+	_putchar(h); 
+	_putchar(a); 
+	_putchar(r); 
+	_putchar(); 
 	return (0);
 }
