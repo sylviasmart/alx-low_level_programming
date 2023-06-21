3a05f5c9c001f51cfc8fb1dc2442984a85b9e393
Slyviasmart <Slyviasmartalozie@gmail.com>
2023-06-20 22:29:41 -0700

    0putcharc

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,6 +5,15 @@
  */
 int main(void)
 {
+	_putchar(_);
+	_putchar(p);
+	_putchar(u);
+	_putchar(t);
+	_putchar(c);
+	_putchar(h);
+	_putchar(a);
+	_putchar(r);
+	_putchar();
 	putchar('\n');
 	return (0);
 }
