87dfee1fc34c3c99892607d345c8547a57da99a2
Slyviasmart <Slyviasmartalozie@gmail.com>
2023-06-26 05:49:11 -0700

    printmostnumbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,19 +7,12 @@
 
 void print_most_numbers(void)
 {
-	int x = 0;
+	char c;
 
-	for (x; <= '9'; x++)
+	for (c = '0'; c <= '9'; c++)
 	{
-		if (x == '2' || x == '4')
-		{
-			continue;
-		}
-		else
-		{
-			_putchar(x + '0');
-		}
+		if (!(c == '2' || c == '4'))
+			_putchar(c);
 	}
-
 	_putchar(\'n');
 }
