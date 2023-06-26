469569a0fc0198d185b4e9f99e3143d3f9e48b65
Slyviasmart <Slyviasmartalozie@gmail.com>
2023-06-25 18:37:37 -0700

    printnumbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers 0 to 9
+ *
+ * Return: output of print 0 to 9
+ */
+void print_numbers(void)
+{
+	int z;
+
+	for (z = 0; z <= 9; z++)
+	{
+		_putchar(z + '0');
+	}
+	_putchar('\n') :
+}
