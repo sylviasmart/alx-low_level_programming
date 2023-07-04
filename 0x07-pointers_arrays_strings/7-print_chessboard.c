8991df02d640646857a1918672c1b48a81d07109
Slyviasmart <Slyviasmartalozie@gmail.com>
2023-07-04 15:25:42 -0700

    chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "string.h"
+
+/**
+* print_chessboard - function to print chessboard
+* @a: array string
+* Return: 0
+*/
+void print_chessboard(char (*a)[8])
+{
+	int y;
+
+	int z;
+
+	for (y = 0; y < 8; y++)
+	{
+		for (z = 0; z < 8; z++)
+		{
+			_putchar(a[y][z]);
+			_putchar('\n');
+		}
+}
+
