6aa290e2eb85f7bcf4d7c70db5ea593cf0ba222b
Slyviasmart <Slyviasmartalozie@gmail.com>
2023-07-17 00:28:50 +0000

    print-alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - Alphabet 10 times
+*
+* Return: 0
+*/
+void print_alphabet_x10(void)
+{
+char c;
+int i = 0;
+
+while (1 <= 9)
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+i++
+}
+}
